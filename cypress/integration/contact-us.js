67ec9267ce56fdb0563927bfac00f56f8c3a68c1
Kristina Romaniuk <Kristina.allegro@gmail.com>
2023-05-22 09:29:32 +0300

    New course udemy update

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress"/>
+
+
+describe("The test Contact us form via webdriveruniversity.com", () => {
+    it("Should be able to a succesfull vsubmission via Contact us form ", () => {
+        //cypress code
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('')
+    });
+
+    it("Should not be able to a succesfull vsubmission via Contact us form as all fields are required", () => {
+        //cypress code
+        cy.
+    })
+}) 
+
+
+
+describe('', () => {
+    it('', () => {
+        
+    });
+});
+
