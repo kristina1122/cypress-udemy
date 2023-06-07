60395f9006b0a396d07b78a43cc57288844f0e33
Kristina Romaniuk <kromaniuk@newage.io>
2023-06-07 13:26:01 +0300

    section 22 - Variables

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -3,7 +3,62 @@
 
 describe("Verifying variables, cypress commands and jquery commands", () => {
     it("Navigating to specific product pages", () => {
-        const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
-        const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare");
+        cy.visit("https://automationteststore.com/");
+        //this will fail
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare");
+        // makeupLink.click();
+        // skincareLink.click();
+
+        //this will pass
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
+        // makeupLink.click();
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare");
+        // skincareLink.click();
+
+        //Recommended Approach 
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click();
+        cy.get("a[href*='product/category&path=']").contains("Skincare").click();
+    });
+
+    it("Navigating to specific product pages", () => {
+        cy.visit("https://automationteststore.com/");
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click();
+ 
+       // the following code will fail
+        // const header = cy.get("h1 .maintext");
+        // cy.log(header.text());
+
+        cy.get("h1 .maintext").then(($headerText) => {
+            const headerText = $headerText.text()
+            cy.log("Found header text: " + headerText);
+            expect(headerText).is.eq.apply('Makeup');
+        })
+
+    });
+
+    it.only("Validate properties of the Contact us Page", () => {
+        cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+
+        //Uses cypress command and chaining
+        cy.contains('#ContactUsFrm', 'Contact Us Form').find('#field_11').should('contain', 'First name');
+
+        //JQuery Approach
+        cy.contains('#ContactUsFrm', 'Contact Us Form').then(text => {
+            const firstNameText = text.find('#field_11').text();
+            expect(firstNameText).to.contain('First name');
+
+             //Embedded commands (Closure)
+             cy.get('#field_11').then(fnText => {
+                cy.log(fnText.text());
+                cy.log(fnText);
+             })
+             
+        })
+
+        //Embedded commands (Closure)
+
+        
+
     });
 })
