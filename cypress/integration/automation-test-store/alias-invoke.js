ebf7d19a5e5ae7be0b432d0ec083def6590d4851
Kristina Romaniuk <kromaniuk@newage.io>
2023-08-09 13:08:45 +0300

    alias + invoke, Multiple browsers

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -35,4 +35,57 @@ describe("Alias and Invoke", () => {
         cy.get('@productThumbnail').find('.productcart').invoke('attr', 'title').should('include', 'Add to Cart');
         // cy.get('@icon').should('include', 'Add to Cart')
     });
-})
+
+    it("Calculate total of normal and sale products", () => {
+        cy.visit("https://automationteststore.com/");
+        cy.get('.thumbnail').as('productThumbnail');
+        // cy.get('@productThumbnail').find('.oneprice').each(($el, index, $list) => {
+        //    this query show only products with full price, without any discount
+        //    cy.log($el.text());
+
+        // });
+        cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice')
+        cy.get('.thumbnail').find('.pricenew').invoke('text').as('saleItemPrice')
+
+        var itemsTotal = 0; 
+        cy.get('@itemPrice').then($linkText => {
+            var itemsPriceTotal = 0;
+            var itemPrice = $linkText.split('$');
+            var i;
+            for(i = 0; i < itemPrice.length; i++) {
+                cy.log(itemPrice[i])
+                itemsPriceTotal += Number(itemPrice[i])
+            }
+            itemsPriceTotal += itemsPriceTotal;
+            cy.log("Non sale price items total: " + itemsPriceTotal)
+        });
+        // cy.get('@saleItemPrice').then($linkText => {
+            // var saleItemPrice = 0;
+            // var saleItemPrice = $linkText.split('$');
+            // var i;
+            // for(i = 0; i < saleItemPrice.length; i++) {
+                // cy.log(saleItemPrice[i])
+                // saleItemPrice += Number(saleItemPrice[i]) 
+            // }
+            // cy.log("Sale price items total: " + saleItemPrice)
+        //  })
+
+        cy.get('@saleItemPrice').then($linkText => {
+            var saleItemsPrice = 0;
+            var saleItemPrice = $linkText.split('$');
+            var i;
+            for(i = 0; i < saleItemPrice.length; i++) {
+                cy.log(saleItemPrice[i])
+                saleItemsPrice += Number(saleItemPrice[i])
+            }
+            itemsTotal += saleItemsPrice;
+            cy.log("Sale price items total: " + saleItemsPrice)
+        })
+        .then(() => {
+            cy.log("The total price of all products: " + itemsTotal)
+            expect(itemsTotal).to.equal(403)
+        })
+    
+
+    });
+});
