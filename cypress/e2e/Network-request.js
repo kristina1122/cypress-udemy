46427d38e9623e1f1fa03a98c58ebc460138db5b
Kristina Romaniuk <kromaniuk@newage.io>
2023-08-22 09:34:18 +0300

    PUT network request

--- cypress/e2e/Network-request.js
+++ cypress/e2e/Network-request.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Network Request", () => {
+    let message = "Unable to find comment!"
     beforeEach(() => {
         cy.visit("https://example.cypress.io/commands/network-requests");
     })
@@ -23,7 +24,7 @@ describe("Network Request", () => {
     cy.wait("@getComment").its("response.statusCode").should("eq", 200)
     });
 
-    it.only("Post Request",() => {
+    it("Post Request",() => {
         cy.intercept("POST", "/comments").as("postComment");
 
         cy.get(".network-post").click();
@@ -34,8 +35,25 @@ describe("Network Request", () => {
             console.log(response);
             expect(response.body).to.have.property("name", "Using POST in cy.intercept()");
             expect(request.headers).to.have.property("content-type");
-            expect(request.headers).to.have.property("Origin", "https://example.cypress.io");
+            expect(request.headers).to.have.property("referer", "https://example.cypress.io/");
         })
 
+    });
+
+    it.only("Put Request", () => {
+        cy.intercept({
+            method: "PUT",
+            url: "**/comments/*"
+        },
+        {
+            statusCode: 404,
+            body: {error: message},
+            delay: 500
+        }).as("putComment");
+
+        cy.get(".network-put").click();
+
+        cy.wait("@putComment");
+        cy.get(".network-put-comment").should("contain", message)
     })
 })
